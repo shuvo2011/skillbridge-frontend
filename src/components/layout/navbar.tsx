74b7a5741c76e722a724e7294ac4af2e8ec656f9
arifuddincoder <shuvombstu2011@gmail.com>
2026-03-06 23:15:58 +0600

    feat: add header and footer components, customize header

---
+++ src/components/layout/navbar.tsx
@@ -0,0 +1,31 @@
+import { Button } from "@/components/ui/button";
+import { Logo } from "../common/logo";
+import { NavMenu } from "./nav-menu";
+import { NavigationSheet } from "./navigation-sheet";
+
+const Navbar = () => {
+	return (
+		<nav className="h-16 bg-brand-peach">
+			<div className="mx-auto flex h-full max-w-(--breakpoint-xl) items-center justify-between px-4 sm:px-6 lg:px-8">
+				<Logo />
+
+				{/* Desktop Menu */}
+				<NavMenu className="hidden md:block" />
+
+				<div className="flex items-center gap-3">
+					<Button className="hidden sm:inline-flex" variant="outline">
+						Sign In
+					</Button>
+					<Button>Get Started</Button>
+
+					{/* Mobile Menu */}
+					<div className="md:hidden">
+						<NavigationSheet />
+					</div>
+				</div>
+			</div>
+		</nav>
+	);
+};
+
+export default Navbar;
