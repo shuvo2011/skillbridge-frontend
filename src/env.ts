152715560e5a9f33ee1778bddae8ebd2920a1a12
arifuddincoder <shuvombstu2011@gmail.com>
2026-04-02 18:05:35 +0600

    add NEXT_PUBLIC_API_URL to env config and update auth client

--- src/env.ts
+++ src/env.ts
@@ -3,14 +3,17 @@ import * as z from "zod";
 
 export const env = createEnv({
 	server: {
-		FRONTEND_URL: z.url(),
-		API_URL: z.url(),
-		AUTH_URL: z.url(),
+		FRONTEND_URL: z.string().url(),
+		API_URL: z.string().url(),
+		AUTH_URL: z.string().url(),
 	},
-
+	client: {
+		NEXT_PUBLIC_API_URL: z.string().url(),
+	},
 	runtimeEnv: {
 		FRONTEND_URL: process.env.FRONTEND_URL,
 		API_URL: process.env.API_URL,
 		AUTH_URL: process.env.AUTH_URL,
+		NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL,
 	},
 });
--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,7 +1,8 @@
+import { env } from "@/env";
 import { inferAdditionalFields } from "better-auth/client/plugins";
 import { createAuthClient } from "better-auth/react";
 export const authClient = createAuthClient({
-	baseURL: process.env.NEXT_PUBLIC_API_URL ?? "http://localhost:5050",
+	baseURL: env.NEXT_PUBLIC_API_URL,
 	plugins: [
 		inferAdditionalFields({
 			user: {
